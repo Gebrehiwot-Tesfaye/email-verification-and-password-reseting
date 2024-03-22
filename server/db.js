4e54dc09ff6ba8eaa0c1c8088c155e883b51d5b3
gebby123 <tefayegebrehiwot123@gmail.com>
2024-03-22 15:18:05 +0300

    email verification and password reseating

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+//erB4QJDnWffRgoRj password
+module.exports = () => {
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+	try {
+		mongoose.connect(process.env.DB, connectionParams);
+		console.log("Connected to database successfully");
+	} catch (error) {
+		console.log(error);
+		console.log("Could not connect database!");
+	}
+};
